8cf72ad9c396ac829c17bcf137317cff12e66efb
mysearchtool1 <ilyaseljaouhari123@gmail.com>
2025-07-25 09:45:28 +0100

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-@app.post("/")
-async def respond():
-    return JSONResponse(content={"verificationToken": "my-verification-token-123"})
+@app.get("/")
+async def read_root():
+    return {"message": "eBay endpoint is live 🎉"}
+
+@app.post("/webhook")
+async def handle_webhook(request: Request):
+    body = await request.body()
+    print(body)
+    return {"status": "received"}
+
